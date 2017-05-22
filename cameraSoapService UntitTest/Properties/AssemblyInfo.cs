8dcc27b464e32e637c6d59cfabaf7676a8e808e3
Atena Nasiri <aten0004@edu.easj.dk>
2017-05-22 16:14:47 +0200

    Add project files.

---
+++ cameraSoapService UntitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("cameraSoapService UntitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cameraSoapService UntitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa6d62a0-0601-4b06-b901-99cbc7486225")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ cameraSoapService UntitTest/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace cameraSoapService_UntitTest
+{
+    /// <summary>
+    /// This Unit test project is testing the CameraSoapService 
+    /// Atena
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        //public CameraSoapDevService.IService1 Service1 { get; set; }
+
+        //[TestInitialize]
+        //public void Initialize()
+        //{
+        //    Service1 = new CameraSoapDevService.IService1();
+        //}
+        [TestMethod]
+        public void TestGetImages()
+        {
+            using(ServiceReference1.Service1Client client=new ServiceReference1.Service1Client("BasicHttpsBinding_IService1"))
+            {
+
+                //Arrange
+                var ImgList ="";
+
+                //Act
+                ImgList = client.GetLatestImage();
+
+                //Assert
+                Assert.IsNotNull(ImgList);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetBookings()
+        {
+            using (ServiceReference1.Service1Client client = new ServiceReference1.Service1Client("BasicHttpsBinding_IService1"))
+            {
+                //Arrange
+                IList<ServiceReference1.Booking> Blist = null;
+
+                //Act
+                Blist = client.GetBookings();
+
+                //Assert
+                Assert.IsNotNull(Blist);
+            }
+        }
+
+        [TestMethod]
+        public void TestAddBooking()
+        {
+            using (ServiceReference1.Service1Client client = new ServiceReference1.Service1Client("BasicHttpsBinding_IService1"))
+            {
+                //Arrange
+                int rowAfected = 0;
+
+                //Act
+                rowAfected = client.SaveBooking("Test SoapService");
+
+                //Assert
+                Assert.AreEqual(rowAfected,1);
+            }
+        }
+
+    }
+}
